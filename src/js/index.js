78a978d0e938673e4f477cec3f22bf9ebaa6c23e
Vladimir Savelyev <rostber@yandex.ru>
2019-09-14 00:20:28 +0300

    Datepicker

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,48 @@ import $ from "jquery";
 window.jQuery = $;
 require('@fancyapps/fancybox/dist/jquery.fancybox');
 require('slick-carousel');
+require('jquery-ui-bundle');
 
 $(function() {
   init_gallery();
   init_zoom();
+  init_order();
 });
+
+function init_order() {
+  var eventDates = ['20190914']
+  $('.js-datepicker').datepicker({
+    closeText: "Закрыть",
+    prevText: "&#x3C;Пред",
+    nextText: "След&#x3E;",
+    currentText: "Сегодня",
+    monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
+    "Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+    monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
+    "Июл","Авг","Сен","Окт","Ноя","Дек" ],
+    dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
+    dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
+    dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+    weekHeader: "Нед",
+    dateFormat: "dd.mm.yy",
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: "",
+    minDate: 0,
+    beforeShowDay: function( date ) {
+      var str = date.toISOString().slice(0,10).replace(/-/g,"");
+      if (eventDates.indexOf(str) > -1) {
+        return [true, 'event', 'highlight'];
+      } else {
+        return [true, '', ''];
+      }
+    },
+    onSelect: function(selectedDate) {
+      console.log('select', selectedDate)
+    }
+  });
+}
 
 function init_zoom() {
   $('.js-zoom').fancybox();
